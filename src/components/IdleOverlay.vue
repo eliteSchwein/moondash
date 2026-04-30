c983f2b32b811cf5db3906216ea7d1aee284acca
tludwig <business@tludwig.dev>
2026-04-30 18:32:34 +0200

    fix: add removed style back feat: add more elements to lock screen if it loads longer then the real screen

--- src/components/IdleOverlay.vue
+++ src/components/IdleOverlay.vue
@@ -2,6 +2,9 @@
 import { onBeforeUnmount, onMounted, watch } from 'vue'
 import { listen, type UnlistenFn } from '@tauri-apps/api/event'
 import { useAppStore } from '../stores/app'
+import {useI18n} from "vue-i18n";
+
+const { t } = useI18n()
 
 type DisplaySleepStatePayload = {
   sleeping: boolean
@@ -39,5 +42,30 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div class="idle-overlay" v-if="appStore.isSleeping" />
+  <div
+      class="idle-overlay d-flex flex-column align-center justify-center text-center"
+      v-if="appStore.isSleeping"
+  >
+    <v-icon
+        icon="mdi-lock-clock"
+        size="64"
+        class="mb-4"
+    />
+
+    <div class="text-h6 font-weight-bold">
+      {{ t('lock.locked') }}
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.idle-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(var(--v-theme-background), 0.8);
+  z-index: 9999;
+}
+</style>
